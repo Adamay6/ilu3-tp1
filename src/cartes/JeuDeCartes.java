ba54b67602f887f353fa44d535e589b0b5400a22
Adamay6 <douma.ayadi58@gmail.com>
2026-09-23 17:42:18 +0200

    testsFoncitonnel added and implemented

--- src/cartes/JeuDeCartes.java
+++ src/cartes/JeuDeCartes.java
@@ -4,7 +4,31 @@ public class JeuDeCartes {
 	
 	private Configuration[] typesDeCartes = new Configuration[19];
 	
-    public String affichageJeuDeCartes() {
+		
+	
+    public JeuDeCartes() {
+    	typesDeCartes[0] = new Configuration(new Borne(25), 10);
+        typesDeCartes[1] = new Configuration(new Borne(50), 10);
+        typesDeCartes[2] = new Configuration(new Borne(75), 10);
+        typesDeCartes[3] = new Configuration(new Borne(100), 12);
+        typesDeCartes[4] = new Configuration(new Borne(200), 4);
+        typesDeCartes[5] = new Configuration(new Parade(Type.FEU), 14);
+        typesDeCartes[6] = new Configuration(new Parade(Type.ESSENCE), 6);
+        typesDeCartes[7] = new Configuration(new Parade(Type.CREVAISON), 6);
+        typesDeCartes[8] = new Configuration(new Parade(Type.ACCIDENT), 6);
+        typesDeCartes[9] = new Configuration(new FinLimite(), 6);
+        typesDeCartes[10] = new Configuration(new DebutLimite(), 4);
+        typesDeCartes[11] = new Configuration(new Attaque(Type.FEU), 5);
+        typesDeCartes[12] = new Configuration(new Attaque(Type.ESSENCE), 3);
+        typesDeCartes[13] = new Configuration(new Attaque(Type.CREVAISON), 3);
+        typesDeCartes[14] = new Configuration(new Attaque(Type.ACCIDENT), 3);
+        typesDeCartes[15] = new Configuration(new Botte(Type.FEU), 1);
+        typesDeCartes[16] = new Configuration(new Botte(Type.ESSENCE), 1);
+        typesDeCartes[17] = new Configuration(new Botte(Type.CREVAISON), 1);
+        typesDeCartes[18] = new Configuration(new Botte(Type.ACCIDENT), 1);
+	}
+
+	public String affichageJeuDeCartes() {
         StringBuilder sb = new StringBuilder();
         sb.append("JEU :\n");
         for (Configuration conf : typesDeCartes) {
