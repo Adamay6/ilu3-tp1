d75b8d37a908cd0ee283d61b18692e219586ad0c
Adamay6 <douma.ayadi58@gmail.com>
2026-09-23 17:33:37 +0200

    method affichageJeuDeCarte updated

--- src/cartes/JeuDeCartes.java
+++ src/cartes/JeuDeCartes.java
@@ -4,9 +4,31 @@ public class JeuDeCartes {
 	
 	private Configuration[] typesDeCartes = new Configuration[19];
 	
-	public String affichageJeuDeCartes() {
-		return "";
-	}
+    public String affichageJeuDeCartes() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("JEU :\n\n");
+        
+        int lignesMax = 7; 
+        
+        for (int i = 0; i < lignesMax; i++) {
+
+            if (i < typesDeCartes.length && typesDeCartes[i] != null) {
+                sb.append(String.format("%-25s", typesDeCartes[i].getNbExemplaires() + " " + typesDeCartes[i].getCarte()));
+            }
+
+            if (i + 7 < typesDeCartes.length && typesDeCartes[i + 7] != null) {
+                sb.append(String.format("%-25s", typesDeCartes[i + 7].getNbExemplaires() + " " + typesDeCartes[i + 7].getCarte()));
+            }
+
+            if (i + 14 < typesDeCartes.length && typesDeCartes[i + 14] != null) {
+                sb.append(typesDeCartes[i + 14].getNbExemplaires() + " " + typesDeCartes[i + 14].getCarte());
+            }
+            sb.append("\n");
+        }
+        
+        return sb.toString();
+    }
+
 	
 	private static class Configuration {
 		private int nbExemplaires;
